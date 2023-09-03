ff7043e149ddac47b2f5ba582463abfb40757e60
vandemberg lima <vandemberg.silva.lima@gmail.com>
2023-09-02 21:51:03 -0300

    Start react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import Home from "./pages/Home";
+import Layout from "./pages/Application/Layout";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-const App = () => <Home buttonName="What do I want?" />;
+const App = function () {
+  return (
+    <Layout>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </Layout>
+  );
+};
 
 export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+// create a component react typescrpit
+import { FC } from "react";
+
+const Home: FC = () => {
+  return <p> Hello world </p>;
+};
+
+export default Home;
